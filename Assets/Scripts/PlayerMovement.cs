d01c863aeb295dab4f0b09750ecb2f18fda37f8a
Jarmzie <jna0420@aol.com>
2021-02-18 09:23:41 -0600

    Basic Stuff

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1;
+    public bool facingRight = false;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator an;
+    private float velocityX = 0.0f, velocityY = 0.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        an = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (velocityX > 0 && !facingRight || velocityX < 0 && facingRight)
+        {
+            sr.flipX = !sr.flipX;
+            facingRight = !facingRight;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        velocityX = Input.GetAxis("Horizontal");
+        velocityY = Input.GetAxis("Vertical");
+        an.SetFloat("VertSpeed", velocityY);
+        rb.velocity = new Vector2(velocityX * speed, velocityY * speed);
+    }
+}

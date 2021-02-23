5575367a1e8b7728e3268d5b887debfc36b7dbd1
noshkren11 <78551523+noshkren11@users.noreply.github.com>
2021-02-23 09:28:07 -0600

    Oil bar

---
+++ Assets/OilBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OilBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxOil(int oil)
+    {
+        slider.maxValue = oil;
+        slider.value = oil;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetOil(int oil)
+    {
+        slider.value = oil;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}

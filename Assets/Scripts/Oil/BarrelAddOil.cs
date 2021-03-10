ec884427f6ded3c40580b49e6e75d0c41bb767a4
noshkren11 <78551523+noshkren11@users.noreply.github.com>
2021-03-10 14:19:53 -0600

    Oil Pickup/ Oil destroy

---
+++ Assets/Scripts/Oil/BarrelAddOil.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelAddOil : MonoBehaviour
+{
+    Collider2D col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject thePlayer = GameObject.Find("Player (Legs)");
+        PlayerAimWeapon playerAimWeapon = thePlayer.GetComponent<PlayerAimWeapon>();
+        
+
+        if (col.gameObject.CompareTag("Player"))
+        {
+            playerAimWeapon.currentOil += 100;
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.CompareTag("PlayerProjectile"))
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+}

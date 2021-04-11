5bc9a3be26111c8ce71e7d569477627a9a088681
Jarmzie <jna0420@aol.com>
2021-04-11 01:49:31 -0500

    Oil Use Upgrade Done

--- Assets/Scripts/Player/PlayerUpgradeController.cs
+++ Assets/Scripts/Player/PlayerUpgradeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class PlayerUpgradeController : MonoBehaviour
 {
@@ -10,5 +11,18 @@ public class PlayerUpgradeController : MonoBehaviour
     {
         PlayerOilController temp = GetComponent<PlayerOilController>();
         temp.OilBar.SetMaxOil(temp.maxOil + (25 * oilAmountUpgrades));
+    }
+
+    public void UpdateOilUsage()
+    {
+        PlayerOilController temp = GetComponent<PlayerOilController>();
+        temp.CancelInvoke();
+        temp.InvokeRepeating("LoseOilOverTime", 0, 1 + (0.1f * oilUseUpgrade));
+    }
+
+    public void UpdateLightRadius()
+    {
+        Light2D temp = GetComponent<Light2D>();
+        //Do light shit
     }
 }

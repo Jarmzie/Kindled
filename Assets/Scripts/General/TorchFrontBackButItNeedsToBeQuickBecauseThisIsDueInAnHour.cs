7eeddf5742e7d76d9be672c2eda7fa5bcc2ace29
Jarmzie <jna0420@aol.com>
2021-04-28 23:08:36 -0500

    puuuush

---
+++ Assets/Scripts/General/TorchFrontBackButItNeedsToBeQuickBecauseThisIsDueInAnHour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchFrontBackButItNeedsToBeQuickBecauseThisIsDueInAnHour : MonoBehaviour
+{
+    [SerializeField]
+    float posX = 0, posY = 0;
+    SpriteRenderer sr;
+    GameObject player;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        player = GameObject.Find("Player (Legs)");
+    }
+
+    public void FixedUpdate()
+    {
+        if (player.transform.position.y > transform.position.y + posY)
+        {
+            sr.sortingLayerName = "EnemyInFront";
+        }
+        else
+        {
+            sr.sortingLayerName = "EnemyInBack";
+        }
+    }
+}

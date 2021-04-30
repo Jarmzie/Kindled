4f522ac2a93f18f38401957819c229733408ee2c
Jarmzie <jna0420@aol.com>
2021-04-30 08:34:34 -0500

    Changed Bjartur finished dialogue

--- Assets/Scripts/Interactables/InteractShopWindow.cs
+++ Assets/Scripts/Interactables/InteractShopWindow.cs
@@ -68,7 +68,10 @@ public class InteractShopWindow : Interactable
                 tempDialogue.GetComponent<Dialogue>().RunDialogue(MOTH_NAME, new string[] {
                     "Welcome back. Made any progress in the dungeon yet?",
                     "Wait! You actually made it to the bottom?! You must be lying to me!",
-                    "So there wasn't actually anything down there? Huh. It looks like you got a pretty interesting looking lantern though.",
+                    "So you turned on a generator? Huh.",
+                    "Wait! That might've been why my TV suddenly turned on!",
+                    "That thing's been out of commision for the longest time. But I guess that means the generator had nothing to do with the lake.",
+                    "It looks like you got a pretty interesting looking lantern though.",
                     "I guess I can keep that here for you. Looks intense.",
                     "Anyway, I guess we have to start thinking of another thing to blame for the oil lake drying up.",
                     "In the meantime, I'll still be around to let you switch out your lanterns."
@@ -94,12 +97,14 @@ public class InteractShopWindow : Interactable
                 break;
             case HubStateManager.ShopState.SkippedSecondLoad2FinalLoad:
                 tempDialogue.GetComponent<Dialogue>().RunDialogue(MOTH_NAME, new string[] {
-                    "Holy $#*@! You just did the whole damn thing in one sweep!",
+                    "Holy $#*@! You just did the whole dang thing in one sweep!",
                     "I guess I really misjudged you, you didn't even need my help!",
                     "I see you have multiple new lanterns. Well I'm gonna let you have full use of my shop!",
                     "Just keep all your lanterns here and I'll let you come by and get them whenever you want. I'll even through in my own personal lantern.",
                     "But wait... The oil lake hasn't returned. Does that mean that the dungeon didn't have the solution?",
-                    "Damn. Welp, I guess we'll have to find another thing to blame for our problems.",
+                    "You turned on the generator? Oh! That must've been why my TV suddenly started working.",
+                    "That thing's been out of commision for the longest time. But I guess that means the generator had nothing to do with the lake.",
+                    "Dang. Welp, I guess we'll have to find another thing to blame for our problems.",
                     "In the meantime, I'll still be around to let you switch out your lanterns.",
                     "Also thanks for sequence breaking the game. Because of you it makes the time put into making these special dialogue trees worth it."
                 }, gameObject, "shop");
@@ -111,9 +116,11 @@ public class InteractShopWindow : Interactable
                 tempDialogue.GetComponent<Dialogue>().RunDialogue(MOTH_NAME, new string[] {
                     "Welcome back. Made any progress in the dungeon yet?",
                     "Wait! You actually made it to the bottom?! You must be lying to me!",
-                    "I see you have multiple new lanterns. Well I guess that must proove that you've at least made some progress.",
+                    "I see you have multiple new lanterns. Well I guess that must prove that you've at least made some progress.",
                     "I'll take those lanterns off your hands. You can come by and take them to use in the dungeon any time.",
                     "But since you've been to the bottom now, I guess that means the dungeon didn't hold the reason behind the dried up oil lake.",
+                    "You turned on the generator? Oh! That must've been why my TV suddenly started working.",
+                    "That thing's been out of commision for the longest time. But I guess that means the generator had nothing to do with the lake.",
                     "Anyway, I guess we have to start thinking of another thing to blame for the oil lake drying up.",
                     "In the meantime, I'll still be around to let you switch out your lanterns."
                 }, gameObject, "shop");
